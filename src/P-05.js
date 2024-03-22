8ac8ab7437b07356b416fcf34308d221a5eab7e4
brooklin <arguetgd@gmail.com>
2024-03-22 21:53:21 +0100

    en esta entrega se encuenta la segunda tercera parte de el laboratorio 3

---
+++ src/P-05.js
@@ -0,0 +1,9 @@
+// Función para ordenar frutas
+const sortFruits = (fruits) => fruits.sort();
+
+// Función para obtener el elemento en el medio de una lista
+const getElementInTheMiddle = (list) => list[Math.floor(list.length / 2)];
+
+// Función para verificar el inicio de sesión del administrador
+const adminLogin = (user) => user === "ADMIN" || user === "DEV";
+
